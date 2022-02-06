561f10c687578a4c63e0270e8a9cdafda0abe71e
Ridam1810 <rithyamforbe@gmail.com>
2022-02-06 22:08:42 +0600

    .

--- update.php
+++ update.php
@@ -5,17 +5,20 @@ include'config.php';
 if (isset($_POST['done'])) {
 
     $id=$_GET['id'];
-    $email = $_POST['email'];
-    $Password = $_POST['pass'];
-    $Comment = $_POST['comment'];
-    $gender = $_POST['gender'];
-    $qualification = $_POST['qualification'];
-    $Hobby = $_POST['hobby'];
+    $first = $_POST['first'];
+        $last = $_POST['last'];
+        $emp_id = $_POST['emp_id'];
+        $gender = $_POST['gender'];
+        $uname = $_POST['uname'];
+        $int = $_POST['int'];
+        $number = $_POST['number'];
+        $comm = $_POST['comm'];
+        $agree = $_POST['agree'];
     //$sql = "INSERT INTO `post` ( `username`, `password`,`comment`,`Gender`,`Qualification`,`Hobby`) VALUES ( '$email', '$Password','$Comment','$gender','$qualification','$Hobby')";
-    $sql ="UPDATE `post` SET `id`='$id',`username`='$email',`password`='Password',`comment`='$Comment',`Gender`='$gender',`Qualification`='$qualification',`Hobby`=' $Hobby' WHERE id=$id";
+    $sql ="UPDATE `post` SET `id`='$id',`first`='$first',`last`='last',`emp_id`='$emp_id',`gender`='$gender',`uname`='$uname',`int`=' $int',`number`=' $number',`comm`=' $comm',`agree`=' $agree' WHERE id=$id";
    // $sql"Udate `post` SET";
     $result = mysqli_query($con, $sql);
-    header('location:Insert.php');
+    
 }
 
 ?>
