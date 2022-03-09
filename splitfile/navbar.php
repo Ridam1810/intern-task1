d900bf729b426f2eb0bef652801e68f27010ccef
Fahad Test <fahad.test@icddrb.net>
2022-03-09 12:30:35 +0600

    changes part

---
+++ splitfile/navbar.php
@@ -0,0 +1,48 @@
+<!-- navbar -->
+<!-- <style>
+    body {
+      background-image: url('img/k.jpg');
+    }
+    </style> -->
+<!-- <div class="container-fluid sticky-top">
+    <div class="row bg-light">
+      <div class=" col-6 text-center m-auto"><a href="index.php" class="btn">
+          <h1 class="text-info">Icddrb</h1>
+        </a> </div>
+      <div class="col-6  mt-3">
+        <form action="" method="POST">
+
+          <div class="row ml-5">
+            
+            <a href="list.php" class="btn btn-outline-info mr-2">List</a>
+            <a href="index.php" class="btn btn-outline-info mr-2">Add New</a>
+
+          </div>
+
+        </form>
+      </div>
+    </div>
+  </div> -->
+  
+  <nav class="navbar navbar-expand-lg navbar-blue  bg-dark">
+    <div class="container-fluid">
+    
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link" href="./index.php"style="color: black;">icddrb</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="./list.php" class="container-fluid justify-content-start" class="btn btn-outline-success me-2">List</a>
+          </li>
+         
+          
+          
+          
+        </ul>
+      </div>
+    </div>
+  </nav>
