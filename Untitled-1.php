44780a479a212e3da010a3ce4fb613c5f03c7df6
Fahad Test <rithyamforbe@gmail.com>
2022-04-06 13:30:20 +0600

    Email system added

--- Untitled-1.php
+++ Untitled-1.php
@@ -86,3 +86,10 @@ include "init.php";
 </body>
 
 </html>
+
+
+
+SMTP=smtp.gmail.com
+smtp_port=587
+sendmail_from = mail@gmail.com
+sendmail_path = "\"C:\xampp\sendmail\sendmail.exe\" -t"
--- deleteguest.php
+++ deleteguest.php
@@ -1,12 +1,17 @@
 <?php
+if (!isset($_SESSION)) {
+  session_start();
+}
 include "init.php";
 include "validation.php";
-
+$path="Uploads/".$_SESSION['filename']; 
+unlink($path);
 if (!isset($_SESSION['username'])) {
   header("Location: login.php");
 }
-else{
 
+else{
+    
     $query = $source->Query("DELETE FROM guest where id=?",[$_GET['deleteuser']]);
 if($query){
     $_SESSION['delete_user'] = "";
